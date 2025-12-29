ead4677cd92712311a7bc37a6f36390109fe8885
Yaroslav <62301460+yaroslav20568@users.noreply.github.com>
2025-12-29 13:56:31 -0500

    fix(general): update import

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from 'next';
+import path from 'path';
+
+const sassPath = process.env.SASS_PATH || 'src';
 
 const nextConfig: NextConfig = {
-  reactCompiler: true
+  reactCompiler: true,
+  sassOptions: {
+    includePaths: [path.join(process.cwd(), sassPath)]
+  }
 };
 
 export default nextConfig;
